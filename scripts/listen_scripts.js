a86f0ba06f72075f755babc3034368faf6237956
bolatashim <bolat.ashim@gmail.com>
2017-05-07 15:59:28 +0900

    added css, scripts folders and modified index.html page to display the courses directory represented using vertical tabs

---
+++ scripts/listen_scripts.js
@@ -0,0 +1,23 @@
+
+$( document ).ready(function() {
+  //Toggle active & inactive states
+  $("#cs101tab").click(function () {
+    if (!$(this).hasClass("listen-active-tab")) {
+      $("#cs101tab").toggleClass("listen-active-tab listen-inactive-tab");
+      $("#cs101content").toggleClass("listen-active-content listen-inactive-content");
+      $("#cs374tab").toggleClass("listen-inactive-tab listen-active-tab");
+      $("#cs374content").toggleClass("listen-inactive-content listen-active-content"); 
+    }
+  });
+
+  $("#cs374tab").click(function () {
+    if(!$(this).hasClass("listen-active-tab")) {
+      $("#cs374tab").toggleClass("listen-active-tab listen-inactive-tab");
+      $("#cs374content").toggleClass("listen-active-content listen-inactive-content");
+      $("#cs101tab").toggleClass("listen-inactive-tab listen-active-tab");
+      $("#cs101content").toggleClass("listen-inactive-content listen-active-content");
+    }
+  });
+});
+
+
