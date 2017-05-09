99b66941d002a7c1aa9873d8a0bc46801c6c2482
tsook <kimts0830@gmail.com>
2017-05-10 02:36:51 +0900

    Changes to js code for active lecture to allow for sorting (sorting not yet implemented)

--- scripts/active_lecture_script.js
+++ scripts/active_lecture_script.js
@@ -1,38 +1,44 @@
-var questions = [{"text": "Question 1 What are for loops used for?", "answered": false},
-				{"text": "Question 2 Why does Hubo not move like I want him to?", "answered": false},
-				{"text": "Question 3 What's the purpose of creating objects?", "answered": false},
-				{"text": "Question 4 Random question because idk what to ask. Lengthened to a 2 line question", "answered": false},
-				{"text": "Question 5 What's the point of using while loops?", "answered": false},
-				{"text": "Question 6 What's the point of using while loops?", "answered": false},
-				{"text": "Question 7 What's the point of using while loops?", "answered": false},
-				{"text": "Question 8 What's the point of using while loops?", "answered": false},
-				{"text": "Question 9 What's the point of using while loops?", "answered": false}];
+var notAnsweredQuestions = [{"text": "Question 1 What are for loops used for?", "time": 1, "understand": true},
+				{"text": "Question 2 Why does Hubo not move like I want him to?", "time": 2, "understand": false},
+				{"text": "Question 3 What's the purpose of creating objects?", "time": 3, "understand": false},
+				{"text": "Question 4 Random question because idk what to ask. Lengthened to a 2 line question", "time": 4, "understand": false},
+				{"text": "Question 5 What's the point of using while loops?", "time": 5, "understand": true},
+				{"text": "Question 6 What's the point of using while loops?", "time": 6, "understand": false},
+				{"text": "Question 7 What's the point of using while loops?", "time": 7, "understand": true},
+				{"text": "Question 8 What's the point of using while loops?", "time": 8, "understand": false},
+				{"text": "Question 9 What's the point of using while loops?", "time": 9, "understand": false}];
+var answeredQuestions = []
 
 $(document).ready(function(){
 	$(".question-list").on("click", ".q-txt", function(){
-		$("#q-" + $(this).data("id")).animate({'opacity': 0}, 100, function(){
-			var del_btn = $(this).children().last();
-			del_btn.html("");
-			del_btn.removeClass("q-del-btn").addClass("q-answered-del");
-			var text = $(this).children().first();
-			text.html("Answered").addClass("q-answered-txt").css({"text-align": "center", "border-width": "1px 0px 1px 1px"});
-			$(this).animate({'opacity': 1}, 100).delay(300);
+		if(!$("#q-" + $(this).data("id")).data("answered")){
+			$("#q-" + $(this).data("id")).animate({'opacity': 0}, 100, function(){
+				var del_btn = $(this).children().last();
+				del_btn.html("");
+				del_btn.removeClass("q-del-btn").addClass("q-answered-del");
+				var text = $(this).children().first();
+				text.html("Answered").addClass("q-answered-txt").css({"text-align": "center", "border-width": "1px 0px 1px 1px"});
+				$(this).animate({'opacity': 1}, 100).delay(300);
 
-			var entry = questions[text.data("id")];
-			questions.splice(text.data("id"), 1);
-			entry.answered = true;
-			questions.push(entry);
-			$(this).fadeOut(200, function(){
-				$(this).remove()
-				printQuestions();
+				var entry = notAnsweredQuestions[text.data("id")];
+				notAnsweredQuestions.splice(text.data("id"), 1);
+				answeredQuestions.push(entry);
+				$(this).fadeOut(200, function(){
+					$(this).remove()
+					printQuestions();
+				})
 			})
-		})
+		}
 	})
 
 	$(".question-list").on("click", ".q-del-btn", function(){
 		$("#q-" + $(this).data("id")).fadeOut(200, function(){
-			$(this).remove()
-			questions.splice($(this).data("id"), 1);
+			$(this).remove();
+			if($(this).parent().data("answered")){
+				answeredQuestions.splice($(this).data("id"), 1);
+			}else{
+				notAnsweredQuestions.splice($(this).data("id"), 1);
+			}
 			printQuestions();
 		});
 	})
@@ -51,14 +57,26 @@ $(document).ready(function(){
 
 function printQuestions(){
 	$(".question-list").empty();
-	var index = questions.length;
-	if(!$(".show-more-btn").data("expanded"))
-		index = 5;
-	for(var i = 0; i < index; i++){
-		if(questions[i].answered){
-			$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt q-answered-txt" data-id="${i}">${questions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+	if(!$(".show-more-btn").data("expanded")){
+		if(notAnsweredQuestions.length < 5){
+			var index = 5 - notAnsweredQuestions.length;
+			for(var i = 0; i < notAnsweredQuestions.length; i++){
+				$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt" data-id="${i}">${notAnsweredQuestions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+			}
+			for(var i = 0; i < index; i++){
+				$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt q-answered-txt" data-id="${i}">${answeredQuestions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+			}
 		}else{
-			$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt" data-id="${i}">${questions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+			for(var i = 0; i < 5; i++){
+				$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt" data-id="${i}">${notAnsweredQuestions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+			}
 		}
+	}else{
+			for(var i = 0; i < notAnsweredQuestions.length; i++){
+				$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt" data-id="${i}">${notAnsweredQuestions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+			}
+			for(var i = 0; i < answeredQuestions.length; i++){
+				$(".question-list").append(`<tr class="question-entry" id="q-${i}"><td class="q-txt q-answered-txt" data-id="${i}">${answeredQuestions[i].text}</td><td class="q-del-btn" data-id="${i}">&#10006;</td></tr>`);
+			}
 	}
 }
