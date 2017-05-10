faae806f54587461dcd1132d46bf3ee87e3c899b
jhw123 <hyoungwookjin123@gmail.com>
2017-05-11 01:16:31 +0900

    created a simple calender and tried to add drag&drop to add_lecture page but dont work

--- scripts/add_lecture_script.js
+++ scripts/add_lecture_script.js
@@ -1,5 +1,5 @@
 var topics = [];
-var topicHTMLst = '<tr><td height="35px"></td><td><div class="topic_name">'
+var topicHTMLst = '<tr><td height="35px"></td><td><div id="topic" class="topic_name" contenteditable="true">'
 var topicHTMLmid = '</div><button class="delete" id="'
 var topicHTMLend = '">&#10006</button></td></tr>'
 
@@ -18,15 +18,23 @@ $(document).ready(function(){
 		topics.splice($(this).attr("id"), 1);
 		printList();
 	})
+
+	$("#topic").on("click", "button", function(){
+		$(this).select();
+	})
 })
 
+$(function(){
+	$("table").tableDnD();
+})
 
 function addTopic(){
 	var topic = $("#input_topic").val();
 	if(topic){
 		$("#topic_list").append(topicHTMLst + topic + topicHTMLmid + topics.length +  topicHTMLend);
 		topics.push(topic);
-		$("#input_topic").val("");
+		$("#input_topic").val("add your topics here");
+		$("#input_topic").select();
 		$("#input_topic").focus();
 	}
 }
