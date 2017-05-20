d6140be86310672a733802b4cfb3acf5b35d7a4a
bolatashim <bolat.ashim@gmail.com>
2017-05-20 19:31:50 +0900

    firebase connected

--- scripts/home_script.js
+++ scripts/home_script.js
@@ -1,37 +1,17 @@
 
 $( document ).ready(function() {
 
-  var config = {
-    apiKey: "AIzaSyCxnL1UyMBU51tJU5MAKmCxHPAaMpb2veY",
-    authDomain: "listen-f5fcf.firebaseapp.com",
-    databaseURL: "https://listen-f5fcf.firebaseio.com",
-    projectId: "listen-f5fcf",
-    storageBucket: "listen-f5fcf.appspot.com",
-    messagingSenderId: "913421957842"
-  };
-  
-  firebase.initializeApp(config);
-
-  var courseKey = "none";
-  var lectureKey = "none";
 
 
+  $(".course-card").click(function() {
+    var code = $(this).children(".course-card-code").text();
+    var title = $(this).children(".course-card-title").text();
 
-  var database = firebase.database();
-  var activelectureRef = database.ref("activeLecture")
-  var courseRef = database.ref("courses/" + courseKey);
-  var lectureRef = database.ref("courses/" + courseKey + "/lectures" + lectureKey);
+    localStorage.courseCode = code;
+    localStorage.courseTitle = title;
 
-
-
-  function addLecture(number, title) {
-    lectureKey = "lecture " + number;
-    lectureRef.set({title: title});
-    
-  }
-
-
-
+    document.location.href = './lectures_list_page.html';
+  });
 
 });
 
