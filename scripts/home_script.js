5be0ad33f1e6b1a5f51942c9e80550b974adc1c3
bolatashim <bolat.ashim@gmail.com>
2017-05-22 18:54:32 +0900

    some fiddling

--- scripts/home_script.js
+++ scripts/home_script.js
@@ -2,7 +2,6 @@
 $( document ).ready(function() {
 
 
-
   $(".clickable-course-card").click(function() {
     var code = $(this).children(".course-card-code").text();
     var title = $(this).children(".course-card-title").text();
@@ -12,7 +11,6 @@ $( document ).ready(function() {
 
     document.location.href = './lectures_list_page.html';
   });
-
 
   $(".clickingbad-course-card").click(function() {
   	alert("It is not 'Add a course' period. Please refer to team Listen() for assistance.");
